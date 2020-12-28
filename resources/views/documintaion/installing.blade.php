42670576c59daee4a725c7163e735c1dd3892395
moazmarouf <61554238+moazmarouf@users.noreply.github.com>
2020-12-28 04:14:55 +0200

    first commit

---
+++ resources/views/documintaion/installing.blade.php
@@ -0,0 +1,5 @@
+** install project **
+    * composer global require laravel/installer => to install packedge . 1
+    * laravel new blog => to install project version latest . 2
+    * composer create-project --prefer-dist laravel/laravel:^7.0 blog => install project version 7 . 2
+
---
+++ resources/views/documintaion/notes.blade.php
@@ -0,0 +1,4 @@
+                                        *** Notes ***
+* file gitignore there is in it folders dont upload when you upload on git because anyone write composer install
+   upload with him .
+* git init for my codes Always present for git
